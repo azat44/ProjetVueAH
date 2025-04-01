39deaf27e23db260639fba4631f2fcfbcbe29d8f
azat44 <azat4beats@gmail.com>
2025-04-01 11:11:41 +0200

    testing increment decrement

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
+import { ref } from 'vue'
+
+let counters = ref([]);
+
+function incrementCounter() {
+}
+
+function decrementCounter() {
+}
+
+function addCounter() {
+  counters.value.push(ref(0));
+}
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="p-4">
+    <h1>Home</h1>
+
+    <div v-for="(counter, index) in counters" :key="index">
+      <div class="border-2 p-2 bg-gray-200">
+        Compteur : {{ counter }}
+      </div>
+
+      <button class="bg-blue-400 rounded-lg px-3 py-1" @click="incrementCounter">Incrémenter</button>
+      <button class="bg-green-400 rounded-lg px-3 py-1" @click="decrementCounter">Décrémenter</button>
+    </div>
+
+    <button class="bg-green-400 rounded-lg px-3 py-1" @click="addCounter">Ajouter un compteur</button>
   </main>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +11,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
